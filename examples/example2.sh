fb1d94689b54d35510edae2c4ecc3c22d6b1e335
Anh Nguyen <anh.ng8@gmail.com>
2016-05-23 14:31:03 -0600

    Updated

--- examples/example2.sh
+++ examples/example2.sh
@@ -1,6 +1,8 @@
+export LD_LIBRARY_PATH=/home/anh/local/lib:/usr/local/cuda-7.5/targets/x86_64-linux/lib:/home/anh/anaconda/lib:${LD_LIBRARY_PATH}
+
 python cnn_vis.py \
   --image_type=amplify_layer \
-  --target_layer=inception_4c/output \
+  --target_layer=conv5 \
   --gpu=0 \
   --num_steps=100 \
   --batch_size=25 \
@@ -10,9 +12,12 @@ python cnn_vis.py \
   --alpha=6.0 \
   --p_reg=1e-10 \
   --beta=2.5 \
-  --tv_reg=5e-1 \
+  --tv_reg=5e-2 \
   --initial_size=270x480 \
-  --final_size=1080x1920 \
-  --num_sizes=5 \
+  --final_size=270x480 \
+  --num_sizes=2 \
   --iter_behavior=print \
-  --output_file=example2.png
+  --output_file=alexnet_conv2.png \
+  --caffe_model="/home/anh/workspace/upconvnet/encoder.caffemodel" \
+  --deploy_txt="/home/anh/workspace/upconvnet/caffenet/encoder.prototxt" \
+  --mean_image="/home/anh/src/caffe_latest/python/caffe/imagenet/ilsvrc_2012_mean.npy"
